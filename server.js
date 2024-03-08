4a95dbe2a6f568a09167b6bfc7b62a8cc70bffb3
ALC <briancalvariomunozq@gmail.comgit config -- global user.name BrianCalvariogit config --global user.email briancalvariomunozq@gmail.com>
2024-03-08 15:21:18 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.listen(4000, ()=>{
+console.log("Yeii tu servidor funciona, estimada Barbara Garcia")
+});
+export default app;
